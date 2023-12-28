2dab9a3ed57b726513770b97fe065d79657015d7
edurodrigues0 <edusacramento9@gmail.com>
2023-12-28 20:13:34 -0300

    Adicionando App Error e tratando exceções

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express'
+import { AppError } from '../utils/errors/AppError'
 
 class UsersController {
   create(request: Request, response: Response) {
     const { name, email, password } = request.body
+
+    if(!name) {
+      throw new AppError("Nome obrigatorio!");
+    }
 
     response.status(201).json({ name, email, password })
   }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,30 @@
-import express from 'express'
+import 'express-async-errors'
+
+import express, { NextFunction, Request, Response } from 'express'
 import routes from './routes'
+
+import { AppError } from './utils/errors/AppError'
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use(( error: Error, request: Request, response: Response, __next: NextFunction ) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Sever is running on Port ${PORT}`))
