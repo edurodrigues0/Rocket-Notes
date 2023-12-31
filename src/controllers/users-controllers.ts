e5c3eec621ea4ff62a75b801b577da60883cbda7
edurodrigues0 <edusacramento9@gmail.com>
2023-12-31 17:18:16 -0300

    Adicionando a funcionalidade atualizar usuario

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -28,6 +28,44 @@ class UsersController {
 
     response.status(201).json()
   }
+
+  async update(request: Request, response: Response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get(
+      'SELECT * FROM users WHERE id = (?)',
+      [id]
+    )
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado!')
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Este e-mail ja esta em uso')
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(`
+      UPDATE users SET 
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.status(200).json()
+  }
 }
 
 export default UsersController
