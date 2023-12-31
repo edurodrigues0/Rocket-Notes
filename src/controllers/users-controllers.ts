a1bfa315860c876f772b67e05f7dc9487f683506
edurodrigues0 <edusacramento9@gmail.com>
2023-12-31 15:40:08 -0300

    Adicionando funcionalidade cadastrar usuario

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from 'express'
 import { AppError } from '../utils/errors/AppError'
+import { sqliteConnection } from '../database/sqlite';
+import { v4 as uuidv4 } from 'uuid'
 
 class UsersController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
     const { name, email, password } = request.body
 
-    if(!name) {
-      throw new AppError("Nome obrigatorio!");
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if(checkUserExists) {
+      throw new AppError('E-mail já está em uso.')
     }
 
-    response.status(201).json({ name, email, password })
+    const id = uuidv4()
+    console.log(id)
+
+    await database.run(
+      'INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)',
+      [id, name, email, password]
+    )
+
+    response.status(201).json()
   }
 }
 
